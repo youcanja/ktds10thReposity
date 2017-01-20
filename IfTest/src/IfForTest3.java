ac69f3d3d022355ee3fa8ef7e3b965135554197e
Min Chang Jang <mcjang1116@gmail.com>
2017-01-20 17:08:41 +0900

    Continue

---
+++ IfTest/src/IfForTest3.java
@@ -0,0 +1,18 @@
+
+public class IfForTest3 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 1 부터 100 사이에 존재하는 모든 짝수를 출력하기
+		 */
+		// 1. 1 부터 100까지 반복 한다.
+		for ( int i = 1; i < 101; i+=2  ) {
+			System.out.println(i);
+		}
+		
+		
+		
+	}
+	
+}
