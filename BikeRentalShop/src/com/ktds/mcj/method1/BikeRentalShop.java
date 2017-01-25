d13cfbda54f1cf2f4efe789faebaf9d3ee7be192
Min Chang Jang <mcjang1116@gmail.com>
2017-01-25 15:17:54 +0900

    no message

---
+++ BikeRentalShop/src/com/ktds/mcj/method1/BikeRentalShop.java
@@ -0,0 +1,56 @@
+package com.ktds.mcj.method1;
+
+public class BikeRentalShop {
+
+	private final int PRICE = 5000;
+
+	private int bikeStock;
+	private int money;
+
+	public BikeRentalShop(int money) {
+		this.bikeStock = 100;
+		this.money = money;
+	}
+
+	public int getBikeStock() {
+		return bikeStock;
+	}
+
+	public void setBikeStock(int bikeStock) {
+		this.bikeStock = bikeStock;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+	/**
+	 * 대여점이 사용자에게 빌려줌
+	 * @param bike
+	 * @param user
+	 */
+	public void rent(int bike, User user) {
+		this.bikeStock -= bike;
+		
+		user.borrow(bike);
+		user.pay(PRICE * bike);
+		
+		money += PRICE * bike;
+	}
+	
+	@Override
+	public String toString() {
+		String message = "===================================================";
+		message += "\n	자전거 대여점 현황";
+		message += "\n—————————————————————————————————------------------";
+		message += String.format("\n	자전거 보유 수 : %d대", this.bikeStock);
+		message += String.format("\n	대여료 총 합 : %d원", this.money);
+		message += "\n===================================================";
+		return message;
+	}
+
+}
