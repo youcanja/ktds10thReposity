00c67abc035d678f7da5d083e79676a23490397d
Min Chang Jang <mcjang1116@gmail.com>
2017-01-26 15:49:10 +0900

    no message

---
+++ File/src/com/ktds/mcj/read/FileRead.java
@@ -0,0 +1,47 @@
+package com.ktds.mcj.read;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead {
+
+	private final String LOG_FOLDER_PATH = "/Users/minchangjang/Desktop/logs";
+	private final String LOG_FILE_PATH = LOG_FOLDER_PATH + "/eclipse_update_120 3.jpg";
+	
+	public void start() {
+		
+		try {
+			FileReader fr = new FileReader(LOG_FILE_PATH);
+			BufferedReader br = new BufferedReader(fr);
+			
+			try {
+				
+				String line = null;
+				while ( true ) {
+					line = br.readLine();
+					
+					if ( line == null ) {
+						break;
+					}
+					
+					System.out.println(line);
+					FileWriteHelper.log(line);
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		new FileRead().start();
+	}
+	
+}
