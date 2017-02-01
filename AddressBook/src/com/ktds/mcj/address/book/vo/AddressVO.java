c3f64309bb76cc51d6b4e957d6aca81ca962ddd9
Min Chang Jang <mcjang1116@gmail.com>
2017-02-01 11:18:58 +0900

    주소록 만들기

---
+++ AddressBook/src/com/ktds/mcj/address/book/vo/AddressVO.java
@@ -0,0 +1,33 @@
+package com.ktds.mcj.address.book.vo;
+
+public class AddressVO {
+
+	private String name;
+	private String phoneNumber;
+	private String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
